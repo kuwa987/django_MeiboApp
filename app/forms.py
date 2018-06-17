35504daec9842895fa2ab3ba539723b816ca502e
kuwa987 <kwn9876@yahoo.co.jp>
2018-06-18 04:54:24 +0900

    view,その他もろもろの編集

--- app/forms.py
+++ app/forms.py
@@ -5,11 +5,10 @@ class ItemForm(forms.ModelForm):
 
     class Meta:
         model = Item
-        fields = ('name', 'age', 'sex,' 'memo')
+        fields = ('name', 'age', 'sex', 'memo')
         widgets = {
             'name': forms.TextInput(attrs={'placeholder':'記入例：山田　太郎'}),
             'age': forms.NumberInput(attrs={'min':1}),
             'sex': forms.RadioSelect(),
             'memo': forms.Textarea(attrs={'rows':4}),
         }
-        
